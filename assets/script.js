d3a4f98781606de21e5626eccb70479076e606e3
Hugo Cicillini <hugo.cicillini@hotmail.com>
2022-08-26 17:41:22 -0300

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("myNav").style.width = '90%';
+    document.getElementById("myNav").style.height = '90%';
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width = '0%';
+}
+
