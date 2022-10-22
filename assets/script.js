2b2a6c456cf16bb8634ce2488c4970eb06e1d4c7
Hugo Cicillini <hugo.cicillini@hotmail.com>
2022-10-22 20:22:36 -0300

    update

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,11 @@ function openNav(){
 function closeNav(){
     document.getElementById("myNav").style.width = 0;
 }
+
+if (window.innerWidth < 701) {
+    function openNav(){
+        document.getElementById("myNav").style.width = '100%';
+        document.getElementById("myNav").style.height = '60%';
+        document.getElementById("myNav").style.right = '0';
+    }
+}
